8ba7482294e69ebd041a5ef87c8a1af50af0e32e
axelgschwind <axel.gschwind@t-online.de>
2022-10-19 15:26:18 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,76 @@
+import sys
+from optparse import OptionParser
+from typing import Sequence
+import pandas as pd
+from Bio import AlignIO, SeqRecord
+from Bio.Seq import Seq
+
+
+#Brings allele id to the form A*01:01, i.e. nomenclature for 4 digits without "HLA-"
+def parse_allele_name(allele: str):
+	(locus, parts) = allele.replace("HLA-","").split("*")
+	parts = parts.split(":")
+	if len(parts) == 0 or len(parts) > 4:
+		raise Exception("Cannot parse HLA string " + allele)
+	if len(parts) == 1:
+		parts.append("01")
+
+	return locus + "*" + ":".join(parts[0:2])
+
+#return first allele that matches four digits allele
+def get_protein_sequence(allele, alignments: AlignIO.MultipleSeqAlignment):
+	allele = parse_allele_name(allele)
+	for alignment in alignments:
+		if allele == parse_allele_name(alignment.name):
+			return alignment.seq
+	return Seq("")
+
+#Calculate average Grantham distance for two sequences of equal length
+_grantham_matrix = pd.DataFrame()
+def grantham_distance(seq1: Seq, seq2: Seq):
+	#initialize grantham matrix with values stored in grantham_matrix.tsv
+	global _grantham_matrix
+	if len(_grantham_matrix) == 0:
+		_grantham_matrix = pd.read_csv("data/grantham.tsv", sep="\t", header=0, index_col=0, comment='#')
+
+	if len(seq1) != len(seq2):
+		raise Exception("Alignemnts of equal length are neccessary to calculate Grantham distance!")
+
+	total_length = 0
+	aa_distance = 0
+
+	for index,aa1 in enumerate(seq1):
+		aa2 = seq2[index]
+		#ignore unknown amino acids (i.e. insertions and deletions)
+		if aa1 not in _grantham_matrix.columns or aa2 not in _grantham_matrix.columns:
+			continue
+
+		total_length += 1
+
+		if aa1 != aa2:
+			aa_distance += _grantham_matrix[aa1][aa2]
+
+	return aa_distance/total_length
+
+
+def main(argv):
+	parser = OptionParser(usage="usage: %prog [options] HLA-ALLELE1 HLA-ALLELE2", description="Calculates HLA diversity metrics. Pass allele names according HLA nomenclature.")
+
+
+	(options, args) = parser.parse_args()
+	if(len(args) != 2):
+		raise Exception("Specify two HLA alleles!")
+	allele1 = args[0]
+	allele2 = args[1]
+
+	#aligned AA sequences
+	protein_alignments = AlignIO.read("data/A_prot.msf", "msf")
+
+
+	seq1 = get_protein_sequence(allele1, protein_alignments)
+	seq2 = get_protein_sequence(allele2, protein_alignments)
+
+	print(grantham_distance(seq1, seq2))
+
+if __name__ == "__main__":
+	main(sys.argv)
