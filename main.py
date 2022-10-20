2c581246f19f6f25e803cc4e382f11d98a845d22
axelgschwind <axel.gschwind@t-online.de>
2022-10-20 12:05:00 +0200

    Added binding grooves for MHC complexes

--- main.py
+++ main.py
@@ -17,13 +17,27 @@ def parse_allele_name(allele: str):
 
 	return locus + "*" + ":".join(parts[0:2])
 
+
+#exon coordinates are compatible with IMGT/HLA protein aligments. Exon 2/3 form binding grooves of MHC class I. Exon 2 forms binding groove of MHC class II
+_binding_groove_coords = {
+	"A": slice(26,228),
+	"B": slice(30,241),
+	"C": slice(24,235),
+	"DQA1": slice(27,111),
+	"DQB1": slice(36,127),
+	"DRB1": slice(33,127)
+}
 #return first allele that matches four digits allele
-def get_protein_sequence(allele, alignments: AlignIO.MultipleSeqAlignment):
+def get_protein_sequence(allele, alignments: AlignIO.MultipleSeqAlignment, whole_protein: bool=False):
 	allele = parse_allele_name(allele)
 	for alignment in alignments:
 		if allele == parse_allele_name(alignment.name):
-			return alignment.seq
-	return Seq("")
+			if whole_protein:
+				return alignment.seq
+			return alignment.seq[ _binding_groove_coords[get_hla_locus(allele)] ]
+
+	raise Exception("Could not find alignment for %s" %allele)
+
 
 #Calculate average Grantham distance for two sequences of equal length
 _grantham_matrix = pd.DataFrame()
@@ -57,17 +71,19 @@ def get_hla_locus(name: str):
 	return parts[0]
 
 
+
 def main(argv):
 	parser = OptionParser(usage="usage: %prog [options] HLA-ALLELE1 HLA-ALLELE2", description="Calculates HLA diversity metrics. Pass allele names according HLA nomenclature.")
+	parser.add_option("--whole_protein", action="store_true", default=False, dest="whole_protein", help="Use the whole protein sequence of MHC complex. Otherwise only binding grooves will be considered (Exon 2/3 for MHC class I and exon 2 for class II, respectively).")
 
 	(options, args) = parser.parse_args()
+	
 	if(len(args) != 2):
 		raise Exception("Specify two HLA alleles!")
 	allele1 = args[0]
 	allele2 = args[1]
 
 	#aligned AA sequences
-
 
 	locus = get_hla_locus(allele1)
 	locus2 = get_hla_locus(allele2)
@@ -77,9 +93,8 @@ def main(argv):
 	#IMGT/HLA protein aligments
 	protein_alignments = AlignIO.read("data/%s_prot.msf" % locus, "msf")
 
-	seq1 = get_protein_sequence(allele1, protein_alignments)
-	seq2 = get_protein_sequence(allele2, protein_alignments)
-
+	seq1 = get_protein_sequence(allele1, protein_alignments, options.whole_protein)
+	seq2 = get_protein_sequence(allele2, protein_alignments, options.whole_protein)
 	print(grantham_distance(seq1, seq2))
 
 if __name__ == "__main__":
